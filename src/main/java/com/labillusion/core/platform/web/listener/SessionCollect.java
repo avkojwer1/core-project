4bcd8a75196181dc1e4929f2abe593d3b6610aff
Greg chen <greg.chen@suryani.cn>
2014-10-20 17:36:12 +0800

    v1

---
+++ src/main/java/com/labillusion/core/platform/web/listener/SessionCollect.java
@@ -0,0 +1,39 @@
+package com.labillusion.core.platform.web.listener;
+
+import org.apache.catalina.SessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by greg.chen on 14-10-20.
+ 将Session 托管
+ */
+public class SessionCollect implements HttpSessionListener {
+    private static final Logger logger = LoggerFactory.getLogger(SessionCollect.class);
+
+    private static final Map<String, HttpSession> sessions = new HashMap<String, HttpSession>();
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        HttpSession session = httpSessionEvent.getSession();
+        logger.debug("session created, id=" + session.getId());
+        sessions.put(session.getId(), session);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        sessions.remove(httpSessionEvent.getSession().getId());
+        logger.debug("session destroyed, id=" + httpSessionEvent.getSession().getId());
+    }
+
+    public static HttpSession find(String sessionId) {
+        return sessions.get(sessionId);
+    }
+
+}
